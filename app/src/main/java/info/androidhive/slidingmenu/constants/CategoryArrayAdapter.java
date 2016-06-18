e2f683c8e1123841e18b15ea52009d7ff9f033df
Juan Jimenez <Juan Jimenez>
2016-06-18 05:14:51 +0200

    More refactor

--- app/src/main/java/info/androidhive/slidingmenu/constants/CategoryArrayAdapter.java
+++ app/src/main/java/info/androidhive/slidingmenu/constants/CategoryArrayAdapter.java
@@ -1,38 +1,13 @@
 package info.androidhive.slidingmenu.constants;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Environment;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.List;
 
 import info.androidhive.slidingmenu.CategoryMessage;
 import info.androidhive.slidingmenu.ProductList;
@@ -42,19 +17,13 @@ import info.androidhive.slidingmenu.R;
 public class CategoryArrayAdapter extends ArrayAdapter {
 
     Button chatText;
-    private LinearLayout singleMessageContainer;
     int layout;
-    private String codSubCat;
     private ArrayList<CategoryMessage> categoryMessages;
-    //Guitarras guitarras;
     public void add(CategoryMessage object) {
         categoryMessages.add(object);
         super.add(object);
 
     }
-    /*public CategoryArrayAdapter(int layout){
-		this.layout = layout;
-	}*/
 
     public CategoryArrayAdapter(Context context, int textViewResourceId, int layout, ArrayList<CategoryMessage> categoryMessages) {
         super(context, textViewResourceId);
@@ -78,16 +47,14 @@ public class CategoryArrayAdapter extends ArrayAdapter {
             LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             row = inflater.inflate(layout, parent, false);
         }
-        singleMessageContainer = (LinearLayout) row.findViewById(R.id.singleMessageContainer);
         final CategoryMessage MessageObj = getItem(position);
         chatText = (Button) row.findViewById(R.id.singleMessage);
-        //chatText.setMaxWidth(450);
         chatText.setText(MessageObj.message);
         chatText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
 
-                producto(MessageObj.title, layout, parent);
+                producto(MessageObj.title, layout);
 
             }
         });
@@ -95,10 +62,7 @@ public class CategoryArrayAdapter extends ArrayAdapter {
         return row;
     }
 
-    public void producto(String codSubCat, int layout, ViewGroup parent) {
-        View row;
-        //ViewGroup parent;
-        this.codSubCat = codSubCat;
+    public void producto(String codSubCat, int layout) {
         this.layout = layout;
         Fragment fragment = null;
         fragment = new ProductList(R.layout.fragment_subcategory,codSubCat);
--- app/src/main/java/info/androidhive/slidingmenu/constants/SubCategoryArrayAdapter.java
+++ app/src/main/java/info/androidhive/slidingmenu/constants/SubCategoryArrayAdapter.java
@@ -1,14 +1,8 @@
 package info.androidhive.slidingmenu.constants;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,17 +19,12 @@ public class SubCategoryArrayAdapter extends ArrayAdapter {
     TextView chatText;
     private LinearLayout singleMessageContainer;
     int layout;
-    int textViewResourceId;
-    private String codSubCat;
     private ArrayList<CategoryMessage> categoryMessages;
     public void add(CategoryMessage object) {
         categoryMessages.add(object);
         super.add(object);
 
     }
-    /*public CategoryArrayAdapter(int layout){
-		this.layout = layout;
-	}*/
 
     public SubCategoryArrayAdapter(Context context, int textViewResourceId, int layout, ArrayList<CategoryMessage> categoryMessages) {
         super(context, textViewResourceId);
@@ -62,26 +51,20 @@ public class SubCategoryArrayAdapter extends ArrayAdapter {
         singleMessageContainer = (LinearLayout) row.findViewById(R.id.singleMessageContainer);
         final CategoryMessage MessageObj = getItem(position);
         chatText = (TextView) row.findViewById(R.id.singleMessage);
-        //chatText.setMaxWidth(450);
         chatText.setText(MessageObj.title);
         chatText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
 
-                producto(MessageObj.message, layout, parent);
+                producto(MessageObj.message, layout);
 
             }
         });
         return row;
     }
 
-    public void producto(String codSubCat, int layout, ViewGroup parent) {
-        View row;
-        //ViewGroup parent;
-        this.codSubCat = codSubCat;
+    public void producto(String codSubCat, int layout) {
         this.layout = layout;
-        //layout = R.layote(layout, parent, false);
-        final Context context = parent.getContext();
         Fragment fragment = null;
         fragment = new ProductView(codSubCat);
         if(Constants.manager!= null)
